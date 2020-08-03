913d0857e8adaf89389771c3b1d0a6690bd781cf
Alec <ivnv.jr@gmail.com>
2020-08-03 13:53:24 -0400

    both of them

---
+++ src/public class August{.java
@@ -0,0 +1,5 @@
+public class August{
+	public static void main(String args[]){
+		System.out.println("August!!");
+	}
+}
---
+++ src/public class Summer{.java
@@ -0,0 +1,6 @@
+public class Summer{
+	public static void main(String args []){
+		System.out.prinln("I like Summer");
+		System.out.println("Freedom and Dream!!");
+	}
+}
